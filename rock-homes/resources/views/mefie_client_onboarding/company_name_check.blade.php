a2aefc259ad9e0c628c946b00ff15141851de0e7
James <jamesonyemi@gmail.com>
2021-03-05 12:18:11 +0000

    add two files, company_name_check and email_check, to improve customer experience.

---
+++ rock-homes/resources/views/mefie_client_onboarding/company_name_check.blade.php
@@ -0,0 +1,93 @@
+<script>
+
+ 
+ async function renderUsers() {
+    
+    const url    = `onboarding/sign-up/customers/check-company-name`;
+    let company  =  document.querySelector("#company_name");
+     console.log((company.value));
+     let html = '';
+     
+     if ( company.value.length === 0) {
+         
+        document.getElementById("btn-save").setAttribute("disabled", "disabled");
+         
+        let htmlSegment = `<div class="my-1 mr-1 row animate__animated animate__headShake">
+                                        <span class="badge badge-danger input-group">company name cannot be empty
+                                            <span class="pt-1 pb-2 mx-1 mb-1 border-danger border-left">
+                                                </span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-x" viewBox="0 0 16 16">
+                                                <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
+                                                <path d="M6.146 5.146a.5.5 0 0 1 .708 0L8 6.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 7l1.147 1.146a.5.5 0 0 1-.708.708L8 7.707 6.854 8.854a.5.5 0 1 1-.708-.708L7.293 7 6.146 5.854a.5.5 0 0 1 0-.708z"/>
+                                                </svg>
+                                        </span>
+                                </div>`; 
+        
+                html += htmlSegment;
+        }
+        else {
+                
+            let verify_customer_name_exist = await getUsers(url, company.value);
+                
+                if ( verify_customer_name_exist.length > 0 ) {
+                    
+                    verify_customer_name_exist.some(user =>  {
+                        if ( (user.company_name === company.value ) ) {
+                            
+                            document.getElementById("btn-save").setAttribute("disabled", "disabled");
+
+                            let htmlSegment = `<div class="row animate__animated animate__headShake">
+                                                <div class="mr-3 email">
+                                                    <span class="badge badge-info">${user.company_name} already taken
+                                                        <span class="pt-1 pb-2 mx-1 mb-1 border-info border-left">
+                                                        </span>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-exclamation" viewBox="0 0 16 16">
+                                                            <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
+                                                            <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+                                                            </svg>
+                                                        </span>
+                                                </div>
+                                            </div>`;
+                    
+                            html += htmlSegment;
+                        } 
+                        
+                    
+                    });
+                } 
+                else {
+                    
+                    document.getElementById("btn-save").removeAttribute("disabled", "disabled");
+
+                    let htmlSegment = `<div class="row animate__animated animate__fadeInRightBig">
+                                            <div class="mr-3">
+                                                <span class="badge badge-success">${company.value} is available
+                                                    <span class="pt-1 pb-2 mx-1 mb-1 border-success border-left">
+                                                    </span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-check" viewBox="0 0 16 16">
+                                                    <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
+                                                    <path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                        </div>`;
+                
+                        html += htmlSegment;
+                }
+            
+        }
+        
+        let container = document.querySelector('#check-company-name-exist');
+        container.innerHTML = html;
+    
+}
+ ( () => {
+     
+     ['keydown','keyup', 'change'].forEach( event => {
+         document.getElementById('company_name').addEventListener(event, renderUsers, false);
+     });
+     
+ })();
+
+
+</script>
